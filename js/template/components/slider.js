ec532c63e28d22ff9faaeb4550b35e993ef85361
Romi Halasz <novocainex@gmail.com>
2014-12-07 04:42:21 -0800

    Carousel changes.

--- js/template/components/slider.js
+++ js/template/components/slider.js
@@ -1,54 +1,76 @@
 var slider =
 
-"<ol class=\"carousel-indicators\">" +
-	"<li data-target=\"#myCarousel\" data-slide-to=\"0\" class=\"active\"></li>" +
-	"<li data-target=\"#myCarousel\" data-slide-to=\"1\"></li>" +
-	"<li data-target=\"#myCarousel\" data-slide-to=\"2\"></li>" +
+"<ol class=\"carousel-indicators slider-indicators\">" +
+	"<li data-target=\"#myCarousel\" data-slide-to=\"0\" class=\"active slider-thumb\"></li>" +
+	"<li data-target=\"#myCarousel\" data-slide-to=\"1\" class=\"slider-thumb\"></li>" +
+	"<li data-target=\"#myCarousel\" data-slide-to=\"2\" class=\"slider-thumb\"></li>" +
 "</ol>" +
 
 "<div class=\"carousel-inner\" role=\"listbox\">" +
+
 	"<div class=\"item active\">" +
-		"<div class=\"container\">" +
-			"<div class=\"carousel-caption\">" +
-				"<h1>Web applications</h1>" +
-				"<p>" +
-					"Websites and HTML apps that run in the browser." +
-				"</p>" +
-				"<p>" +
-					"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
-				"</p>" +
+		"<div class=\"carousel-caption\">" +
+			"<div class=\"container\">" +
+				"<div class=\"row\">" +
+					"<div class=\"col-sm-4 col-sm-offset-1\">" +
+						"<img src=\"https://developers.google.com/tv/web/images/hybrid_web_app_main_ui.png\" alt=\"\" class=\"slider-img\">" +
+					"</div>" +
+					"<div class=\"col-sm-6 col-sm-offset-1\">" +
+						"<h1>Web Applications</h1>" +
+						"<p>" +
+							"Websites and HTML/JS apps that run in the browser." +
+						"</p>" +
+						"<p>" +
+							"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
+						"</p>" +
+					"</div>" +
+				"</div>" +
 			"</div>" +
 		"</div>" +
 	"</div>" +
 
 	"<div class=\"item\">" +
-		"<div class=\"container\">" +
-			"<div class=\"carousel-caption\">" +
-				"<h1>Software development</h1>" +
-				"<p>" +
-					"Websites and HTML apps that run in the browser." +
-				"</p>" +
-				"<p>" +
-					"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
-				"</p>" +
+		"<div class=\"carousel-caption\">" +
+			"<div class=\"container\">" +
+				"<div class=\"row\">" +
+					"<div class=\"col-sm-4 col-sm-offset-1\">" +
+						"<img src=\"https://developers.google.com/tv/web/images/hybrid_web_app_main_ui.png\" alt=\"\" class=\"slider-img\">" +
+					"</div>" +
+					"<div class=\"col-sm-6 col-sm-offset-1\">" +
+						"<h1>Software Development</h1>" +
+						"<p>" +
+							"When the line between classic Software Development and Web Apps is fading..." +
+						"</p>" +
+						"<p>" +
+							"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
+						"</p>" +
+					"</div>" +
+				"</div>" +
 			"</div>" +
 		"</div>" +
 	"</div>" +
 
 	"<div class=\"item\">" +
-		"<div class=\"container\">" +
-			"<div class=\"carousel-caption\">" +
-				"<h1>Games</h1>" +
-				"<p>" +
-					"Websites and HTML apps that run in the browser." +
-				"</p>" +
-				"<p>" +
-					"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
-				"</p>" +
+		"<div class=\"carousel-caption\">" +
+			"<div class=\"container\">" +
+				"<div class=\"row\">" +
+					"<div class=\"col-sm-4 col-sm-offset-1\">" +
+						"<img src=\"https://developers.google.com/tv/web/images/hybrid_web_app_main_ui.png\" alt=\"\" class=\"slider-img\">" +
+					"</div>" +
+					"<div class=\"col-sm-6 col-sm-offset-1\">" +
+						"<h1>Games</h1>" +
+						"<p>" +
+							"While being fun to make and to play, games are their own niche." +
+						"</p>" +
+						"<p>" +
+							"<a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">See more</a>" +
+						"</p>" +
+					"</div>" +
+				"</div>" +
 			"</div>" +
 		"</div>" +
 	"</div>" +
-"</div>" +
+"</div>"/* +
 
 "<a class=\"left carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"prev\">" +
 	"<span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>" +
@@ -58,6 +80,6 @@ var slider =
 "<a class=\"right carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"next\">" +
 	"<span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>" +
 	"<span class=\"sr-only\">Next</span>" +
-"</a>";
+"</a>"*/;
 
 var sliderTemplate = Handlebars.compile(slider);
